d5ff9d8b4acaf9bc02b3734e1ac7f5493caeba93
EliClaw56 <89602283+EliClaw56@users.noreply.github.com>
2021-11-18 12:45:51 -0800

    df

--- sketch.js
+++ sketch.js
@@ -3,24 +3,39 @@ let animating = false
 let layout = [];
 let images;
 let button;
+let timerValue = 3;
+let startButton;
+let x = 0;
+let song;
+let myFont;
 
-function preload (){
-  for(let i = 0; i <= 3; i++){
-    layout[i] = loadImage('Layouts/layout_'+i+'.png');
+function preload() {
+  for (let i = 0; i <= 0; i++) {
+    layout[i] = loadImage('Layouts/layout_' + i + '.png');
   }
+  soundFormats('mp3', 'ogg');
+  song = loadSound("03-DigDug_Walking.mp3");
 }
+// }
+//   myFont= loadFont('chalkduster/chalkduster.ttf');
+
 
 function setup() {
-  createCanvas(displayWidth,displayHeight);
+  createCanvas(1000, 500);
   strokeWeight(8);
   console.log(layout);
   imageMode(CENTER);
   background(209, 162, 98);
+  textSize(20);
+  setInterval(timeIt, 1000);
+  // button = select('#genButton')
+  // button.class("randomizerButton");
+  // button.doubleClicked(timeIt);
 
-  button = select('#genButton')
-  button.position(560,200);
-  button.class("randomizerButton");
-  button.doubleClicked(refresh);
+  push();
+  song.loop();
+  pop();
+
 
 }
 
@@ -29,20 +44,39 @@ function draw() {
     push();
     noStroke();
     erase(255);
-    circle(mouseX,mouseY,50);
+    circle(mouseX, mouseY, 50);
     noErase();
     pop();
+  }
 
+  stroke(255);
+  rect(width / 20, 40, 67, 30, 20);
+  push();
+  if (timerValue >= 10) {
+    text("0:" + timerValue, width / 20 + 15, 62);
+  }
+  if (timerValue < 10) {
+    text('0:0' + timerValue, width / 20 + 15, 62);
+  }
+  if (timerValue == 0) {
+    noStroke();
+    textSize(50);
+    // textFont(myFont);
+    text('GAME OVER', width / 3 + 30, height / 2 + 15);
+    noLoop();
+    pop();
   }
 }
-function refresh(){
-  clear();
-  background(209, 162, 98);
+
+function timeIt() {
+  if (timerValue > 0) {
+    timerValue--;
+  }
 }
 
 function keyTyped() {
   if (key === 's') {
     saveCanvas('Untitled', 'jpg');
-}
-    return false;
   }
+  return false;
+}
